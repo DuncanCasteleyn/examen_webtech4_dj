d89a7b968b00552823f52112d5c6fadc3d2165bd
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-09-06 15:47:16 +0200

    Created index page for /movies

--- webtech4_exam/views.py
+++ webtech4_exam/views.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
+
+r_db = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+# Create your views here.
+
+def index(request):
+    result = r_db.hgetall('movies')
+    output = '<body>'
+    for item in result:
+        output += '<p>Movie: ' + item + '<br />Actors: ' + r_db.hget('movies', item) + '</p>'
+    output += '</body>'
+    return HttpResponse(output)
+
