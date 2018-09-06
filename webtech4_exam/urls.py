cb14939a8b89a23647bd9a09c56282a82526e61d
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-09-06 15:48:02 +0200

    Added init and post

--- webtech4_exam/urls.py
+++ webtech4_exam/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^post', views.post, name='add movie'),
+    url(r'^init', views.init, name='init movies'),
     url(r'^', views.index, name='index'),
 ]
--- webtech4_exam/views.py
+++ webtech4_exam/views.py
@@ -18,3 +18,24 @@ def index(request):
     output += '</body>'
     return HttpResponse(output)
 
+
+def post(request):
+    movie_name = request.GET.get('movie')
+    actors = request.GET.get('actors')
+    if movie_name == None or actors == None:
+        return HttpResponseBadRequest('<body>'
+                            '<h1>Missing parameter(s)!</h1>'
+                            '<p>Requires request parameter \'movie\' and \'actors\''
+                            '</body>', )
+    r_db.hset('movies', movie_name, actors)
+    return HttpResponse('<body><h1>added!</h1></body>')
+
+
+def init(request):
+    r_db.hset('movies', 'The Godfather', 'Al Pacino, Marlon Brando, Robert Duvall')
+    r_db.hset('movies', 'Schindler\'s List', 'Liam Neeson, Ralph Fiennes, Ben Kingsley')
+    r_db.hset('movies', 'Saving Private', 'Ryan Tom Hanks, Matt Damon, Vin Diesel')
+    r_db.hset('movies', 'Back to the Future', 'Michael J. Fox, Christopher Lloyd, Lea Thompson')
+    r_db.hset('movies', 'Casablanca', 'Ingrid Bergman, Humphrey Bogart, Peter Lorre')
+    r_db.hset('movies', 'The Big Lebowski', 'Julianne Moore, Jeff Bridges, Tara Reid')
+    return HttpResponse('<body><h1>Default values added!</h1></body>')
